17d1a540d01716d60ca3055e1bf6713bfc8b05ad
Lippiece <seriouss7222@gmail.com>
2022-04-04 12:27:59 +0200

    Record the divs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-let container = document.querySelector("div");
-let divs = [];
+let divs = document.querySelectorAll("div");
+let paintContainer = divs[0];
+let btnContainer = divs[1];
 
 for (let i = 0; i < 16; i++) {
 	let temp = document.createElement("div");
