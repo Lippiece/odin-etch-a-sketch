d137e6a4cb7dc521eca3657b880acac8bfa03908
Lippiece <seriouss7222@gmail.com>
2022-04-04 12:28:25 +0200

    Add an array for paint blocks

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ let divs = document.querySelectorAll("div");
 let paintContainer = divs[0];
 let btnContainer = divs[1];
 
+let paintBlocks = [];
+
 for (let i = 0; i < 16; i++) {
 	let temp = document.createElement("div");
 	divs.push(temp);
